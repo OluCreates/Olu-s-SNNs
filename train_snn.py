a5a786987d61a060bdd46839ed15bfd2ab810afa
OluCreates <112454325+OluCreates@users.noreply.github.com>
2026-07-02 13:18:07 -0700

    Update parameter comments and define SNN class
    Refactor parameter comments and add SNN class structure.

--- train_snn.py
+++ train_snn.py
@@ -5,15 +5,27 @@ from torchvision import datasets, transforms
 import snntorch as snn
 from snntorch import surrogate
 
-//parameters
-batchSize = 128
+#parameters
+batch_size = 128
 learningRate = 1e-3
-epochs = 5 //cycles
+epochs = 5 #cycles
 steps = 25  
 
-//load and process data
+#load and process data
 transform = transforms.Compose([transforms.ToTensor()])
 train_data = datasets.MNIST(root='data', train=True, download=True, transform=transform)
 test_data = datasets.MNIST(root='data', train=False, download=True, transform=transform)
 train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
+
+#SNN Class
+class SNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+      self.fc1 = nn.Linear(784, 256)
+        self.lif1 = snn.Leaky(beta=0.9, spike_grad=surrogate.fast_sigmoid())
+        self.fc2 = nn.Linear(256, 10)
+        self.lif2 = snn.Leaky(beta=0.9, spike_grad=surrogate.fast_sigmoid())
+
+    def forward(self, x):
+        pass
