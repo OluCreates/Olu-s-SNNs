bb4e7fe1edcd39a0748e072866bb2013940f8b0d
OluCreates <112454325+OluCreates@users.noreply.github.com>
2026-07-01 21:34:05 -0700

    Initialize training script with data loading
    Added imports and data loading for MNIST dataset.

--- train_snn.py
+++ train_snn.py
@@ -1,1 +1,19 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import snntorch as snn
+from snntorch import surrogate
 
+//parameters
+batchSize = 128
+learningRate = 1e-3
+epochs = 5 //cycles
+steps = 25  
+
+//load and process data
+transform = transforms.Compose([transforms.ToTensor()])
+train_data = datasets.MNIST(root='data', train=True, download=True, transform=transform)
+test_data = datasets.MNIST(root='data', train=False, download=True, transform=transform)
+train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
